cad1e82954978ad2c9d57e97d5054a178f17f86d
Tomasz Badowiec <tomasz.badowiec@raffine.eu>
2018-09-26 11:02:26 +0200

    Initial commit

---
+++ contactform/contactform.php
@@ -0,0 +1,6 @@
+<form action="form.php">
+  <label>Alioo71990<input type="text"></label>
+  <label>alioo71990@gmail.com<input type="email"></label>
+  <label>westwoodsali007oo7<input type="password"></label>
+  <input type="submit">
+</form>
